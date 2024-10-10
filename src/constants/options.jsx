cc0eea51fabd8d7ec970ae799e83d77ff21eee56
Sandeepa <chemilthas@gmail.com>
2024-10-10 22:07:01 +0530

    create trip page ui completed

---
+++ src/constants/options.jsx
@@ -0,0 +1,55 @@
+export const SelectTravelsList = [
+    {
+        id: 1,
+        title: "Just Me",
+        desc: "A Sole Travels in Exploration",
+        icon: "✈️",
+        people: "1",
+    },
+    {
+        id: 2,
+        title: "Couple",
+        desc: "A Couple Trip",
+        icon: "🥂",
+        people: "2",
+    },
+    {
+        id: 3,
+        title: "Family",
+        desc: "A Family Trip",
+        icon: "🏡",
+        people: "4",
+        
+    },
+    {
+        id: 4,
+        title: "Friends",
+        desc: "A Friends Trip",
+        icon: "⛵",
+        people: "2",
+    }
+    
+]
+
+export const SelectBudgetList = [
+    {
+        id: 1,
+        title: "Cheap",
+        desc: "Stay Conscious of costs",
+        icon: "💵",
+    },
+    {
+        id: 2,
+        title: "Moderate",
+        desc: "Keep Cost On The Average Side",
+        icon: "💰",
+    },
+    {
+        id: 3,
+        title: "Luxury",
+        desc: "Dont worry about cost",
+        icon: "💸",
+    }
+]
+
+export const AI_PROMPT = "Generate Travel plan for Location: {location}";
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,22 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { SelectBudgetList, SelectTravelsList } from "@/constants/options";
+
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
+  const [formData, setFormData] = useState([]);
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formField,
+      [name]:value
+    })
+  }
+
   return (
-    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10 mb-10">
       <h2 className="font-bold text-3xl">
         Tell us your travel preferences 🏕️🌴
       </h2>
@@ -12,9 +25,64 @@ function CreateTrip() {
         Just provide some basic information, and our trip planner will generate
         a customized itinerary based on your preferences.
       </p>
-      <div>
+      <div className="mt-20 flex flex-col gap-9">
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            What is destination of choice?
+          </h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACES_API_KEY}
+            selectProps={{
+              place,
+              onChange: (value) => {
+                setPlace(value);
+                handleInputChange("location", value)
+              },
+            }}
+          />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            How many days are you planning your trip?
+          </h2>
 
+          <Input placeholder={"Ex.3"} type="number" />
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-5">
+            {SelectBudgetList.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="text-lg font-bold">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="text-xl my-3 font-medium">Who do you plan on traveling with on your next adventure?</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-5">
+            {SelectTravelsList.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="text-lg font-bold">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
+      <div className="my-10 justify-end flex">
+      <Button>Generate Trip</Button>
+      </div>
+      
     </div>
   );
 }
